50ebb86dc750acb259339094a16fec3907f2942e
Chris Neal <chris@analogrepublic.com>
2019-07-01 09:01:21 +0100

    Fix lack of styling

--- resources/js/components/ResourceTool.vue
+++ resources/js/components/ResourceTool.vue
@@ -60,60 +60,62 @@
 </script>
 
 <template>
-    <loading-view :loading="loading">
-        <div v-if="!subscriptions || subscriptions.length == 0">
-            <p class="text-90">
-                <em>User has no subscriptions.</em>
-                <!-- <br/>
-                <select v-model="plan" class="form-control form-select">
-                    <option value="" disabled="disabled" selected="selected">Choose Plan</option>
-                    <option :value="plan" v-for="plan in plans">
-                        {{ plan.nickname }} (${{ plan.price / 100 }})
-                    </option>
-                </select><br/>
-                <button @click="createSubscription()" :disabled="!plan" class="btn btn-default btn-primary inline-flex items-center relative ml-auto mr-3">
-                    Subscribe
-                </button> -->
-            </p>
-        </div>
-        <div v-else>
-            <div v-for="subscription in subscriptions" class="subscription-div">
-                <h3>Subscription</h3>
-                <div class="flex border-b border-40" v-if="subscription">
-                    <div class="w-1/4 py-4"><h4 class="font-normal text-80">Plan</h4></div>
-                    <div class="w-3/4 py-4"><p class="text-90">
-                        {{subscription.plan}}
-                        ({{subscription.plan_amount / 100}} {{subscription.plan_currency}} / {{subscription.plan_interval}})
-                    </p></div>
-                </div>
+    <div class="card mb-6 py-3 px-6">
+        <loading-view :loading="loading">
+            <div v-if="!subscriptions || subscriptions.length == 0">
+                <p class="text-90">
+                    <em>User has no subscriptions.</em>
+                    <!-- <br/>
+                    <select v-model="plan" class="form-control form-select">
+                        <option value="" disabled="disabled" selected="selected">Choose Plan</option>
+                        <option :value="plan" v-for="plan in plans">
+                            {{ plan.nickname }} (${{ plan.price / 100 }})
+                        </option>
+                    </select><br/>
+                    <button @click="createSubscription()" :disabled="!plan" class="btn btn-default btn-primary inline-flex items-center relative ml-auto mr-3">
+                        Subscribe
+                    </button> -->
+                </p>
+            </div>
+            <div v-else>
+                <div v-for="subscription in subscriptions" class="subscription-div">
+                    <h3>Subscription</h3>
+                    <div class="flex border-b border-40" v-if="subscription">
+                        <div class="w-1/4 py-4"><h4 class="font-normal text-80">Plan</h4></div>
+                        <div class="w-3/4 py-4"><p class="text-90">
+                            {{subscription.plan}}
+                            ({{subscription.plan_amount / 100}} {{subscription.plan_currency}} / {{subscription.plan_interval}})
+                        </p></div>
+                    </div>
 
-                <div class="flex border-b border-40" v-if="subscription">
-                    <div class="w-1/4 py-4"><h4 class="font-normal text-80">Subscribed since</h4></div>
-                    <div class="w-3/4 py-4"><p class="text-90">{{subscription.created_at}}</p></div>
-                </div>
+                    <div class="flex border-b border-40" v-if="subscription">
+                        <div class="w-1/4 py-4"><h4 class="font-normal text-80">Subscribed since</h4></div>
+                        <div class="w-3/4 py-4"><p class="text-90">{{subscription.created_at}}</p></div>
+                    </div>
 
-                <div class="flex border-b border-40" v-if="subscription">
-                    <div class="w-1/4 py-4"><h4 class="font-normal text-80">Billing Period</h4></div>
-                    <div class="w-3/4 py-4"><p class="text-90">{{subscription.current_period_start}} => {{subscription.current_period_end}}</p></div>
-                </div>
+                    <div class="flex border-b border-40" v-if="subscription">
+                        <div class="w-1/4 py-4"><h4 class="font-normal text-80">Billing Period</h4></div>
+                        <div class="w-3/4 py-4"><p class="text-90">{{subscription.current_period_start}} => {{subscription.current_period_end}}</p></div>
+                    </div>
 
-                <div class="flex border-b border-40 remove-bottom-border" v-if="subscription">
-                    <div class="w-1/4 py-4"><h4 class="font-normal text-80">Status</h4></div>
-                    <div class="w-3/4 py-4">
-                        <p class="text-90">
-                            <span v-if="subscription.on_grace_period">On Grace Period</span>
-                            <span v-if="subscription.cancelled || subscription.cancel_at_period_end" class="text-danger">Cancelled</span>
-                            <span v-if="subscription.active && !subscription.cancelled && !subscription.cancel_at_period_end">Active</span>
-                            ·
-                            <router-link class="text-primary no-underline" :to="'/cashier-tool/user/'+resourceId+'/subscriptions/'+subscription.id">
-                                Manage
-                            </router-link>
-                        </p>
+                    <div class="flex border-b border-40 remove-bottom-border" v-if="subscription">
+                        <div class="w-1/4 py-4"><h4 class="font-normal text-80">Status</h4></div>
+                        <div class="w-3/4 py-4">
+                            <p class="text-90">
+                                <span v-if="subscription.on_grace_period">On Grace Period</span>
+                                <span v-if="subscription.cancelled || subscription.cancel_at_period_end" class="text-danger">Cancelled</span>
+                                <span v-if="subscription.active && !subscription.cancelled && !subscription.cancel_at_period_end">Active</span>
+                                ·
+                                <router-link class="text-primary no-underline" :to="'/cashier-tool/user/'+resourceId+'/subscriptions/'+subscription.id">
+                                    Manage
+                                </router-link>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </loading-view>
+        </loading-view>
+    </div>
 </template>
 
 <style lang="scss">
